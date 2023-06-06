a333caf135e316da25274a94eacc3616c38ee026
Sri-1227 <srinivasmekala1227@gmail.com>
2023-06-07 02:17:20 +0530

    waking hours endpoint

--- wysa-frontend/src/components/QThree.jsx
+++ wysa-frontend/src/components/QThree.jsx
@@ -2,23 +2,21 @@ import arrow from '../assets/arrow-down.svg'
 import { useState } from 'react'
 import {TimePicker} from 'react-ios-time-picker'
 
-
-const QTwo = () => {
-  const [value,setValue]  = useState('10:00')
+const QThree = () => {
+    const [value,setValue]  = useState('10:00')
   const onChange = (timeValue)=>{
     setValue(timeValue)
 
   }
   return (
-    <div className="bg-[#00001d] h-screen rounded-lg text-center p-4">
-        <h2 className="text-white my-4">{"What time do you go to bed for sleep?"}</h2>
+      <div className="bg-[#00001d] h-screen rounded-lg text-center p-4">
+        <h2 className="text-white my-4">{"What time do you get out of bed to start your day?"}</h2>
         <div id="options" className="text-white border rounded-lg">
             <TimePicker  onChange={onChange} value={value}  />    
         </div>
         <div id="submit" className="cursor-pointer m-4 text-center bg-yellow-400 inline-block rounded-full"><img src={arrow} className='cursor-pointer pointer-events-none w-10' alt="" /></div>
-      
     </div>
   )
 }
 
-export default QTwo
+export default QThree
